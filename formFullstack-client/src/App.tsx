94b952835d91ab42f60619f3eac11d1ea48e9a26
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-30 12:55:27 -0300

    add contrl logic in the form

--- formFullstack-client/src/App.tsx
+++ formFullstack-client/src/App.tsx
@@ -1,25 +1,69 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [formState, setFormState] = useState(
+    {
+      name: "",
+      favoriteNumber: 0,
+      file: ""
+    }
+  )
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name
+    let value: string | number = e.target.value
+
+    if (name === 'favoriteNumber') value = parseInt(value)
+    setFormState({ ...formState, [name]: value })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log(formState)
+  }
 
   return (
     <>
       <h1>Formulario</h1>
-      <form>
-        <div>
 
+      <form onSubmit={handleSubmit}>
+
+        <div>
           <label htmlFor='name'>Nombre</label>
           <input
             type='text'
+            name='name'
             placeholder='Nombre'
+            value={formState.name}
+            onChange={onInputChange}
           />
         </div>
-        <div>
 
-          <label htmlFor='picture'>Archivo de imagen</label>
-          <input type='file' />
+        <div>
+          <label htmlFor='name'>Número favorito</label>
+          <input
+            type='number'
+            name='favoriteNumber'
+            placeholder='Número favorito'
+            value={formState.favoriteNumber}
+            onChange={onInputChange}
+          />
         </div>
+
+        <div>
+          <label htmlFor='picture'>Archivo de imagen</label>
+          <input
+            type='file'
+            name='file'
+            value={formState.file}
+            onChange={onInputChange}
+          />
+        </div>
+
         <button>Enviar</button>
+
       </form>
     </>
   )
