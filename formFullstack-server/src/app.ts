1265b7eaee43da02674de2947c42634bb00aaefd
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-29 21:23:44 -0300

    add server

---
+++ formFullstack-server/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/api",(req, res)=>res.send( "Hola"))
+
+app.listen(5000, ()=>console.log('Servidor escuchando en el puerto 5000'))
