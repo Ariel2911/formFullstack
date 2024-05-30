29b024f3b0d82db92e5e5c10941a1ea70cea7f8b
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-30 19:29:15 -0300

    update server

--- formFullstack-server/src/app.ts
+++ formFullstack-server/src/app.ts
@@ -6,6 +6,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/api",(req, res)=>res.send( "Hola"))
+app.get("/api",(req, res)=>res.send({"connection": "ok"}))
 
 app.listen(5000, ()=>console.log('Servidor escuchando en el puerto 5000'))
