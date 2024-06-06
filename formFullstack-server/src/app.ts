9e2d486e9a0e9234e530be822e815797e906a5fc
Ariel Barrios <ariel29111978@hotmail.com>
2024-06-05 23:22:23 -0300

    add new route: /api/solicitud-post.simple

--- formFullstack-server/src/app.ts
+++ formFullstack-server/src/app.ts
@@ -2,10 +2,19 @@ import express from "express";
 import cors from "cors"
 
 const app = express()
+const simplePost: Object[] = []
 
 app.use(cors())
 app.use(express.json())
 
 app.get("/api",(req, res)=>res.send({"connection": "ok"}))
+
+app.post("/api/solicitud-post.simple",(req, res)=>{
+  const data: Object= req.body
+  console.log(data)
+  simplePost.push(data)
+
+  res.send({"solicitud-post.simple": "ok", simplePost})
+})
 
 app.listen(5000, ()=>console.log('Servidor escuchando en el puerto 5000'))
