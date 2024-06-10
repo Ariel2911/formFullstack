d8bfd69c2b65a040b0bcce42276dd0cfc7f14573
Ariel Barrios <ariel29111978@hotmail.com>
2024-06-10 20:44:46 -0300

    add swagger documentation in "/api" endpoint

--- formFullstack-server/src/app.ts
+++ formFullstack-server/src/app.ts
@@ -1,12 +1,28 @@
 import express from "express";
 import cors from "cors"
+import swaggerUi from "swagger-ui-express"
+import swaggerSpecification from "./swagger";
 
 const app = express()
 const simplePost: Object[] = []
 
 app.use(cors())
 app.use(express.json())
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerSpecification))
 
+/**
+ * @openapi
+ * /api:
+ *  get:
+ *    summary: Informa el estado de la comunicación
+ *    tags:
+ *      - Estado
+ *    responses:
+ *      200:
+ *        description: Ok
+ *      500:
+ *        description: Error interno del servidor
+ */
 app.get("/api",(req, res)=>res.send({"connection": "ok"}))
 
 app.post("/api/solicitud-post.simple",(req, res)=>{
